a8727300d0a00d3fdf800e2b1d9ec4670aba6a9a
Shiva <shivasaini.5666@gmail.com>
2024-07-26 21:58:57 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+
+
+
+
